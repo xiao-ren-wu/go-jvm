8266f8d4a4a3be17aa5cfb3fa2f87766a8f2e9ac
xiao-ren-wu <18629015421@163.com>
2019-07-25 23:48:19 +0800

    load class

--- src/jvmgo/ch01/main.go
+++ src/jvmgo/ch01/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"jvmgo/ch02/classpath"
+	"strings"
+)
 
 func main() {
 	cmd := parseCmd()
@@ -12,6 +16,16 @@ func main() {
 		startJVM(cmd)
 	}
 }
+
 func startJVM(cmd *Cmd)  {
+	cp:=classpath.Parse(cmd.XjreOption,cmd.cpOption)
 	fmt.Printf("classpath:%s class: %s args: %v\n",cmd.cpOption,cmd.class,cmd.args)
+	className:=strings.Replace(cmd.class,".","/",-1)
+	classData,_,err:=cp.ReadClass(className)
+	if err!=nil{
+		fmt.Printf("Could not find or load main class %s\n",cmd.class)
+		return
+	}
+	fmt.Printf("class data:%v\n",classData)
+
 }
