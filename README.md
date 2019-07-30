901ce6937d0a50e194f7a1079ba75fbc3ccf03dd
xiao-ren-wu <18629015421@163.com>
2019-07-30 23:56:45 +0800

    ,,

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # go-jvm
 :milky_way: Implementation of Java Virtual Machine through go Language
+
+### class字节码
+1. 魔数  
+class字节码的魔数是："0xCAFEBABY"
+2. 版本号  
+魔数之后是class文件的次版本号和主版本号，如果某class字节码文件的主版本号是M，次版本号是m,那么完整的
--- src/jvmgo/ch03/classfile/class_file.go
+++ src/jvmgo/ch03/classfile/class_file.go
@@ -42,29 +42,34 @@ func (self *ClassFile) read(reader *ClassReader) {
 	self.thisClass = reader.readUnit16()
 	self.superClass = reader.readUnit16()
 	self.interfaces = reader.readUnit16s()
-	self.fields = readMembers(reader,self.constantPool)
-	self.methods =readMembers(reader,self.constantPool)
-	self.attributes = readAttributes(reader,self.constantPool)
+	self.fields = readMembers(reader, self.constantPool)
+	self.methods = readMembers(reader, self.constantPool)
+	self.attributes = readAttributes(reader, self.constantPool)
 }
+
+/**
+魔数检查
+*/
 func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
-	magic:=reader.readUnit32()
-	if magic!=0xCAFEBABE{
+	magic := reader.readUnit32()
+	if magic != 0xCAFEBABE {
 		panic("java.lang.ClassFormatError:magic!")
 	}
 }
+
 /*
 	M.m
 	java 在1.2之前用过次版本号，主版本号为45
 	1.2之后，没用过次版本号每次发布都是主版本号+1
- */
+*/
 func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
 	self.minorVersion = reader.readUnit16()
 	self.majorVersion = reader.readUnit16()
 	switch self.majorVersion {
 	case 45:
 		return
-	case 46,47,48,49,50,51,52:
-		if self.minorVersion==0{
+	case 46, 47, 48, 49, 50, 51, 52:
+		if self.minorVersion == 0 {
 			return
 		}
 	}
@@ -79,6 +84,7 @@ func (self *ClassFile) MajorVersion() uint16 {
 func (self *ClassFile) ConstantPool() ConstantPool {
 
 }
+
 //访问标志：返回该class文件是接口还是类，访问级别是public还是private
 func (self *ClassFile) AccessFlags() uint16 {
 
@@ -93,16 +99,16 @@ func (self *ClassFile) ClassName() string {
 	return self.constantPool.getClassName(self.thisClass)
 }
 func (self *ClassFile) SuperClassName() string {
-	if self.superClass>0{
+	if self.superClass > 0 {
 		return self.constantPool.getClassName(self.superClass)
 	}
 	//只有Java.lang.Object没有超类
 	return ""
 }
 func (self *ClassFile) InterfacesNames() []string {
-	interfaceNames:=make([]string,len(self.interfaces))
-	for i,cpIndex:=range self.interfaces{
-		interfaceNames[i]=self.constantPool.getClassName(cpIndex)
+	interfaceNames := make([]string, len(self.interfaces))
+	for i, cpIndex := range self.interfaces {
+		interfaceNames[i] = self.constantPool.getClassName(cpIndex)
 	}
 	return interfaceNames
 }
